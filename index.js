39c5e7fffc38c0c2260a733aac4b8ab879ef260e
Shan.tk <tksudharshan@gmail.com>
2020-05-30 16:14:19 +0530

    Added KeepAlive Script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const httpProxy = require('http-proxy')
-const express = require('express')
+const express = require('express');
+const keepalive = require('./keepalive');
 const request = require('request')
 const httpsrv = require('httpsrv')
 const fs = require('fs')
@@ -10,7 +11,10 @@ const SECRET = /rpc-secret=(.*)/.exec(
 const ENCODED_SECRET = Buffer.from(SECRET).toString('base64')
 
 const PORT = process.env.PORT || 1234
-const app = express()
+const app = express();
+
+keepalive();
+
 const proxy = httpProxy.createProxyServer({
 	target: 'ws://localhost:6800',
 	ws: true
